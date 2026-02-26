8645d1a4673ef3c7b8943fe863415780b81b841e
FawazTakahji <56781705+FawazTakahji@users.noreply.github.com>
2026-02-26 17:43:15 +0300

    fix android and bump minSDK to 23

--- src/Nyaavigator.Android/MainActivity.cs
+++ src/Nyaavigator.Android/MainActivity.cs
@@ -1,12 +1,7 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Avalonia;
 using Avalonia.Android;
-using Avalonia.Controls;
-using CommunityToolkit.Mvvm.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection;
 using Nyaavigator.Core;
-using Nyaavigator.Core.Extensions;
 
 namespace Nyaavigator.Android;
 
@@ -16,19 +11,4 @@ namespace Nyaavigator.Android;
     Icon = "@drawable/icon",
     MainLauncher = true,
     ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.ScreenSize | ConfigChanges.UiMode)]
-public class MainActivity : AvaloniaMainActivity
-{
-    protected override AppBuilder CustomizeAppBuilder(AppBuilder builder)
-    {
-        if (!Design.IsDesignMode)
-        {
-            Ioc.Default.ConfigureServices(
-                new ServiceCollection()
-                    .AddCoreServices()
-                    .BuildServiceProvider());
-        }
-
-        return base.CustomizeAppBuilder(builder)
-            .WithInterFont();
-    }
-}
+public class MainActivity : AvaloniaMainActivity;

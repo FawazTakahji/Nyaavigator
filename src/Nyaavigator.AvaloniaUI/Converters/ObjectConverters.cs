7ce36d69cde1ef1ffffdadcd8239b7b14f4663a3
FawazTakahji <56781705+FawazTakahji@users.noreply.github.com>
2026-03-12 01:57:51 +0300

    Add EqualConverter multi-value converter
    Introduce EqualConverter (IMultiValueConverter) and an ObjectConverters helper. The converter compares two bound values and returns their equality (bool), or BindingOperations.DoNothing if inputs are missing/invalid. Useful for Avalonia multi-bindings to perform equality checks in XAML.

---
+++ src/Nyaavigator.AvaloniaUI/Converters/ObjectConverters.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using Avalonia.Data;
+using Avalonia.Data.Converters;
+
+namespace Nyaavigator.AvaloniaUI.Converters;
+
+public class ObjectConverters
+{
+    public static readonly EqualConverter Equal = new();
+}
+
+public class EqualConverter : IMultiValueConverter
+{
+    public object? Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (values is not [{ } a, { } b])
+        {
+            return BindingOperations.DoNothing;
+        }
+
+        return a.Equals(b);
+    }
+}

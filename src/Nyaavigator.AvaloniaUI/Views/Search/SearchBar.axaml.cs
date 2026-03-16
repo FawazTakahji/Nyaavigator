3b8d96ae02629c61182049327cb7d044a0e7ff53
FawazTakahji <56781705+FawazTakahji@users.noreply.github.com>
2026-03-16 05:02:22 +0300

    Add filter button to search bar

--- src/Nyaavigator.AvaloniaUI/Views/Search/SearchBar.axaml.cs
+++ src/Nyaavigator.AvaloniaUI/Views/Search/SearchBar.axaml.cs
@@ -1,10 +1,21 @@
-﻿using Avalonia.Controls;
+﻿using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
 using Avalonia.Input;
 
 namespace Nyaavigator.AvaloniaUI.Views.Search;
 
 public partial class SearchBar : UserControl
 {
+    public static readonly StyledProperty<ICommand> ShowSearchSettingsCommandProperty = AvaloniaProperty.Register<SearchBar, ICommand>(
+        nameof(ShowSearchSettingsCommand));
+
+    public ICommand ShowSearchSettingsCommand
+    {
+        get => GetValue(ShowSearchSettingsCommandProperty);
+        set => SetValue(ShowSearchSettingsCommandProperty, value);
+    }
+
     public SearchBar()
     {
         InitializeComponent();
@@ -12,7 +23,7 @@ public partial class SearchBar : UserControl
 
     private void SearchBar_OnGettingFocus(object? sender, FocusChangingEventArgs e)
     {
-        if ((e.NavigationMethod == NavigationMethod.Pointer && (e.Source == MoreButton || e.Source == SearchButton))
+        if ((e.NavigationMethod == NavigationMethod.Pointer && (e.Source == MoreButton || e.Source == SearchButton || e.Source == FilterButton))
             || e is { NavigationMethod: NavigationMethod.Unspecified, OldFocusedElement: MenuItem })
         {
             e.Handled = true;

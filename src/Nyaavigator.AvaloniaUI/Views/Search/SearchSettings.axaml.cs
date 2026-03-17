2691cf30640b1af228dfdb8a68fe5a6a72324c13
FawazTakahji <56781705+FawazTakahji@users.noreply.github.com>
2026-03-17 04:59:16 +0300

    add search options modal

---
+++ src/Nyaavigator.AvaloniaUI/Views/Search/SearchSettings.axaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows.Input;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Interactivity;
+using CommunityToolkit.Mvvm.Input;
+using Nyaavigator.Core.ViewModels;
+using Ursa.Controls.OverlayShared;
+
+namespace Nyaavigator.AvaloniaUI.Views.Search;
+
+public partial class SearchSettings : UserControl
+{
+    public static readonly StyledProperty<ICommand> CloseCommandProperty = AvaloniaProperty.Register<SearchSettings, ICommand>(
+        nameof(CloseCommand));
+
+    public ICommand CloseCommand
+    {
+        get => GetValue(CloseCommandProperty);
+        set => SetValue(CloseCommandProperty, value);
+    }
+
+    public SearchSettings()
+    {
+        InitializeComponent();
+        CloseCommand = new RelayCommand<OverlayFeedbackElement>(Close);
+    }
+
+    private static void Close(OverlayFeedbackElement? owner)
+    {
+        owner?.Close();
+    }
+
+    private void ToggleButton_OnIsCheckedCategoryChanged(object? sender, RoutedEventArgs e)
+    {
+        if (sender is not ToggleButton { IsChecked: false } toggleButton || DataContext is not SearchViewModel viewModel)
+        {
+            return;
+        }
+
+        if (toggleButton.DataContext == viewModel.SelectedCategory.Parent
+            || viewModel.SelectedCategory.Parent is null && toggleButton.DataContext is EmptyObject)
+        {
+            toggleButton.IsChecked = true;
+        }
+    }
+}
--- src/Nyaavigator.AvaloniaUI/Views/Search/SearchView.axaml.cs
+++ src/Nyaavigator.AvaloniaUI/Views/Search/SearchView.axaml.cs
@@ -1,4 +1,8 @@
 ﻿using Avalonia.Controls;
+using CommunityToolkit.Mvvm.Input;
+using Nyaavigator.Core.ViewModels;
+using Ursa.Common;
+using Ursa.Controls;
 
 namespace Nyaavigator.AvaloniaUI.Views.Search;
 
@@ -7,5 +11,25 @@ public partial class SearchView : UserControl
     public SearchView()
     {
         InitializeComponent();
+        Bar.ShowSearchSettingsCommand = new RelayCommand(ShowSearchSettings);
+    }
+
+    private void ShowSearchSettings()
+    {
+        if (DataContext is not SearchViewModel viewModel)
+        {
+            return;
+        }
+
+        Drawer.ShowModal<SearchSettings, SearchViewModel>(viewModel, Host.HostId, new()
+        {
+            Position = Position.Bottom,
+            Buttons = DialogButton.None,
+            CanLightDismiss = true,
+            IsCloseButtonVisible = true,
+            Title = string.Empty,
+            CanResize = false,
+            StyleClass = "SearchSettings"
+        });
     }
 }
